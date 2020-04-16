381447e6f8c034fcd0796738fe2b0573d00f93e2
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-15 14:49:09 -1200

    ADDED: Calculate empwage for month

--- EmployWageComputation.java
+++ EmployWageComputation.java
@@ -1,6 +1,6 @@
 import java.lang.*;
 
-public class EmployWageUC4
+public class EmployWageComputation
 {
    public static void main(String args[])
    {
@@ -8,25 +8,34 @@ public class EmployWageUC4
       final int IS_FULL_TIME=1;
       final int IS_PART_TIME=2;
       final int EMP_RATE_HRS=20;
+      final int WORKING_DAY=20;
       //variable
       int empHrs=0;
       int empWage=0;
-      int attendence=(int)(Math.random()*10)%3;
+      int attendence;
+      int salary=0;
       //find empHr
-      switch(attendence)
+      for(int i=1;i<=20;i++)
       {
-         case IS_FULL_TIME:
-            empHrs=8;
-            break;
-         case IS_PART_TIME:
-            empHrs=4;
-            break;
-         default:
-            empHrs=0;
-            break;
+         attendence=(int)(Math.random()*10)%3;
+         switch(attendence)
+         {
+            case IS_FULL_TIME:
+               empHrs=8;
+               break;
+            case IS_PART_TIME:
+               empHrs=4;
+               break;
+            default:
+               empHrs=0;
+               break;
+         }
+         salary+=empHrs*EMP_RATE_HRS;
       }
+
       //calculate empwage
-      System.out.println("Employ Wage:"+(empHrs*EMP_RATE_HRS));
+      System.out.println("Employ Wage for month : "+salary);
+
    }
 }
 
