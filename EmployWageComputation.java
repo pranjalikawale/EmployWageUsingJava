244cfdb33b9ac7fce40c609d17f8c839db7c47cb
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-15 14:21:45 -1200

    ADDED : Calculate empwage for part time

--- EmployWageComputation.java
+++ EmployWageComputation.java
@@ -1,19 +1,26 @@
 import java.lang.*;
 
-public class EmployWageComputation
+public class EmployWageUC3
 {
-	public static void main(String args[])
-	{
-		final int IS_FULL_TIME=1;
-		final int EMP_RATE_HRS=20;
-		int empHrs=0;
-		int empWage=0;
-		double attendence=Math.floor(Math.random()*10)%2;
-
-		if(attendence == IS_FULL_TIME)
-			empHrs=8;
-		
-		empWage=empHrs*EMP_RATE_HRS;
-		System.out.println("Employ Wage:"+empWage);
-	}
+   public static void main(String args[])
+   {
+      //constant
+      final int IS_FULL_TIME=1;
+      final int IS_PART_TIME=2;
+      final int EMP_RATE_HRS=20;
+      //variable
+      int empHrs=0;
+      int empWage=0;
+      double attendence=Math.floor(Math.random()*10)%3;
+      //find empHr
+      if(attendence == IS_FULL_TIME)
+         empHrs=8;
+      else if(attendence == IS_PART_TIME)
+         empHrs=4;
+      else
+         empHrs=0;
+      //calculate empwage
+      System.out.println("Employ Wage:"+(empHrs*EMP_RATE_HRS));
+   }
 }
+
