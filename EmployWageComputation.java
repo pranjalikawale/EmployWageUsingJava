fe96af27b82924dc144d9c770c2237c4246ae025
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-15 14:15:58 -1200

    ADDED: calculate employwage for full time

--- EmployWageComputation.java
+++ EmployWageComputation.java
@@ -2,14 +2,18 @@ import java.lang.*;
 
 public class EmployWageComputation
 {
-   public static void main(String args[])
-   {
-      int IS_FULL_TIME=1;
-      double attendence=Math.floor(Math.random()*10)%2;
+	public static void main(String args[])
+	{
+		final int IS_FULL_TIME=1;
+		final int EMP_RATE_HRS=20;
+		int empHrs=0;
+		int empWage=0;
+		double attendence=Math.floor(Math.random()*10)%2;
 
-      if(attendence == IS_FULL_TIME)
-         System.out.println("Employee is Present");
-      else
-         System.out.println("Employee is Absent");
-   }
+		if(attendence == IS_FULL_TIME)
+			empHrs=8;
+		
+		empWage=empHrs*EMP_RATE_HRS;
+		System.out.println("Employ Wage:"+empWage);
+	}
 }
