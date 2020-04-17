76868ac7261bdf64e2a2f1186c42483d5d12ab6d
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-16 12:45:07 -1200

    ADDED: Function to get employ hrs

--- EmployWageComputation.java
+++ EmployWageComputation.java
@@ -2,42 +2,56 @@ import java.lang.*;
 
 public class EmployWageComputation
 {
+   //constant
+   final static int IS_FULL_TIME=1;
+   final static int IS_PART_TIME=2;
+   final static int EMP_RATE_HRS=20;
+   final static int WORKING_DAY=20;
+   final static int WORKING_HOURS=100;
+
+   //return empHrs
+   public int getHour(int attendence)
+   {
+      int empHrs=0;
+
+      switch(attendence)
+      {
+         case IS_FULL_TIME:
+            empHrs=8;
+            break;
+         case IS_PART_TIME:
+            empHrs=4;
+            break;
+         default:
+            empHrs=0;
+            break;
+      }
+      return empHrs;
+   }
+
    public static void main(String args[])
    {
-      //constant
-      final int IS_FULL_TIME=1;
-      final int IS_PART_TIME=2;
-      final int EMP_RATE_HRS=20;
-      final int WORKING_DAY=20;
-      final int WORKING_HOURS=100;
+      //create instance
+      EmployWageComputation employ= new EmployWageComputation();
+
       //variable
       int empHrs=0;
       int empWage=0;
-      int attendence;
+      int empAttendence;
       int totalWorkingDay=0;
       int totalWorkingHrs=0;
-      //find empHr
+
+      //find totalWorkingHrs
       while(totalWorkingHrs<WORKING_HOURS && totalWorkingDay<WORKING_DAY)
       {
-         attendence=(int)(Math.random()*10)%3;
-         switch(attendence)
-         {
-            case IS_FULL_TIME:
-               empHrs=8;
-               break;
-            case IS_PART_TIME:
-               empHrs=4;
-               break;
-            default:
-               empHrs=0;
-               break;
-         }
-         totalWorkingHrs=totalWorkingHrs+empHrs;
+         empAttendence=(int)(Math.random()*10)%3;
+         totalWorkingHrs=totalWorkingHrs+employ.getHour(empAttendence);
          totalWorkingDay++;
       }
+
       //calculate empwage
       System.out.println("Employ Wage for month : "+(totalWorkingHrs*EMP_RATE_HRS));
-
    }
 }
+
 
